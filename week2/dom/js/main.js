c005689660dfe8fbd31823987881c95bcac4c081
Luke Hammer <textchimp@gmail.com>
2017-12-14 10:34:48 +1100

    added event handler/animation examples

--- week2/dom/js/main.js
+++ week2/dom/js/main.js
@@ -37,3 +37,26 @@ iWillRunYourFunction( sayHello );
 // button.addEventListener( 'click', sayHello );
 
 button.addEventListener( 'click', sayHello);
+
+
+// get a reference to the DOM node for the image of Bill
+const bill = document.getElementById('bill');
+
+// We need to set this ourselves, because it's not set for us from the CSS
+bill.style.top = '0px';
+
+// A new function we'll use to animate the image of Bill (make it move down)
+const watchBillFall = function() {
+  const oldTop = parseInt(bill.style.top); // get the previous value (just the number)
+  const newTop = oldTop + 10;  // increment it by 10
+  bill.style.top = newTop + 'px';   // set the style with our new value
+  // NOTE: the image had better have 'position: relative' (or absolute) set in the stylesheet for these offsets to be applied
+};
+
+// Add ANOTHER event handler to the button, when clicked
+// We'll do this using an anonymous/inline function, which
+// will just cause the animation interval to start when we click it
+button.addEventListener( 'click', function(){
+  // Call our animation function every 200ms
+  window.setInterval(watchBillFall, 200);
+});

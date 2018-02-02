235da2b98a0e2435717cf4ece8a2fe189d11f9bf
Luke Hammer <textchimp@gmail.com>
2018-02-02 12:19:44 +1100

    React intro app

---
+++ week7/react-intro/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HelloWorld = function(){
+  // return React.createElement('h1', null, 'Hello World!');
+  return (
+    <div>
+     <h1>Hello World!</h1>
+     <img src="http://fillmurray.com/300/200" />
+    </div>
+  );
+};
+
+export default HelloWorld;

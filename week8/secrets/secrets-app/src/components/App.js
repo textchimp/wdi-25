e04318a0ef36dec14a84dafa272aca46ce1ed252
Luke Hammer <textchimp@gmail.com>
2018-02-06 09:57:22 +1100

    Secrets app, Rails and React

---
+++ week8/secrets/secrets-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Secrets from './Secrets';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Secrets />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week8/secrets/secrets-app/src/components/Secrets.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Secrets extends Component {
+
+  render(){
+    return (
+      <div>
+        <h1>Tell Us Your Secrets</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Secrets;
